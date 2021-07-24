2c61a9cfdb264a184c8384a582fd0c941c5c3b78
Jose Postiga <jpostiga@infraspeak.com>
2021-07-24 12:54:43 +0100

    build: removed unnecessary docker image

--- .docker/php/compile.sh
+++ .docker/php/compile.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 docker build 80-fpm-prod -t ghcr.io/josepostiga/syllabus/php
 docker build 80-fpm-dev -t ghcr.io/josepostiga/syllabus/php:dev
-docker build 80-fpm-cov -t ghcr.io/josepostiga/syllabus/php:cov
