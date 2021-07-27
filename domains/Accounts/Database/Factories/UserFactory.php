5ec92608c9af269926edc2b3394b905fbcf09b15
Jose Postiga <jpostiga@infraspeak.com>
2021-07-27 18:07:58 +0100

    fix(Accounts): add missing not nullable column definition

--- domains/Accounts/Database/Factories/UserFactory.php
+++ domains/Accounts/Database/Factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Domains\Accounts\Database\Factories;
 
+use Domains\Accounts\Enums\UserRolesEnum;
 use Domains\Accounts\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -19,6 +20,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'role' => UserRolesEnum::DIRECTOR,
         ];
     }
 
