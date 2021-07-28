bc50095932f0d0e687a833ed4d5f1e2b8a7eeb00
Jose Postiga <jpostiga@infraspeak.com>
2021-07-28 08:14:14 +0100

    fix(Auth): language var reference

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -32,7 +32,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => __('auth.failed'),
+                'email' => __('accounts::auth.errors.failed'),
             ]);
         }
 
