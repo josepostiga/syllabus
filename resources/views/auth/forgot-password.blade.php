d24b10e166b98d2d0d82a78ae8769f8dcafca761
José Postiga <jpostiga@infraspeak.com>
2021-08-21 18:52:13 +0100

    refactor: auth and general components

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -11,10 +11,10 @@
         </div>
 
         <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')"/>
+        <x-alert-banner class="mb-4" :status="session('status')"/>
 
         <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors"/>
+        <x-validation-errors class="mb-4" :errors="$errors"/>
 
         <form method="POST" action="{{ route('password.email') }}">
         @csrf
