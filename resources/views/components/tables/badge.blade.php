ec299421ce299720faaf788f35cf34037ee26d08
José Postiga <jpostiga@infraspeak.com>
2021-08-31 07:49:57 +0100

    wip(Users): refactor component variable delimeter

--- resources/views/components/tables/badge.blade.php
+++ resources/views/components/tables/badge.blade.php
@@ -4,6 +4,6 @@
   $status = $condition === true ? 'bg-green-100' : 'bg-red-100';
 @endphp
 
-<span {{ $attributes->merge(['class' => 'px-2 inline-flex leading-5 font-semibold rounded-full text-green-800 '.$status]) }}>
+<span {{ $attributes->merge(['class' => "px-2 inline-flex leading-5 font-semibold rounded-full text-green-800 {$status}"]) }}>
   {{ __('properties.statuses.active') }}
 </span>
